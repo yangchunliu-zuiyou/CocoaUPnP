9982ba452470e69411d9453b2f7088eb4392d98a
Simon Yang <mmkwycl@gmail.com>
2025-07-15 17:45:27 +0800

    add dependencies: CocoaAsyncSocket, AFNetworking, GCDWebServer; Use Ono source code

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
@@ -7,17 +7,31 @@ let package = Package(
         .iOS(.v11)
     ],
     products: [
-        .library(name: "CocoaUPnP", targets: ["CocoaUPnP"])
+        .library(
+            name: "CocoaUPnP",
+            targets: ["CocoaUPnP"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4")
+        // .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4"),
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", branch: "master"),
+        .package(url: "https://github.com/AFNetworking/AFNetworking", from: "4.0.1"),
+        // .package(url: "https://github.com/swisspol/GCDWebServer.git", from: "3.6.3") // or your forked version
+        .package(url: "https://github.com/yene/GCDWebServer.git", from: "3.5.7")
     ],
     targets: [
         .target(
             name: "CocoaUPnP",
-            dependencies: ["CocoaAsyncSocket"],
-            path: "Sources/CocoaUPnP",
-            publicHeadersPath: "."
+            dependencies: [
+                "CocoaAsyncSocket",
+                .product(name: "AFNetworking", package: "AFNetworking"),
+                "GCDWebServer"
+            ],
+            path: "Sources/CocoaUPnP"//,
+            // publicHeadersPath: "../../include/CocoaUPnP",
+            // cSettings: [
+            //     .headerSearchPath("../../include")
+            // ]
         )
     ]
 )
